1915775e109918213d6c31467549fdee5800bd06
Tom C <thomasecopas@gmail.com>
2026-02-20 02:11:23 -0500

    played around with getting items from directories.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::env::current_dir;
 use std::fs;
 use std::io;
+use std::env;
+use std::path::PathBuf;
 
 struct Node {
     is_dir: bool,
@@ -26,16 +29,40 @@ fn create_node(){}
 fn dir_object_count(){}
 
 //get the contents of the given directory
-fn parse_dir(){}
+fn parse_dir(dir:PathBuf){
+    let items = fs::read_dir(dir).unwrap();
+    for item in items {
+        //we can get the filename from the path, which of course we need to unwrap, but we can then display
+        println!("Name: {}", item.unwrap().path().file_name().unwrap().display())
+    }
 
-//Need to support command line args, namely "-a" for getting all sub directories 
-fn main() {
-    println!("Hello, world!");
+}
 
+
+
+//Need to support command line args, namely "-a" for getting all sub directories (Eventually)
+fn main() -> Result<(), std::io::Error>{
     //initial thrust is getting the backend put together. 
     //We can use a vec of a custom struct
     //that holds the name, size in bits, type (file or directory, probably 0/1), index of parent,  
     //and a starting index in the vec itself if it is a directory.
 
     //[0,1,2,3...n]
+
+    //get current directory of where the command is being invoked
+    let cwd:PathBuf = current_dir()?;
+    //We can get the current working directory using current_dir(), which returns a Result we need to unpack using "?"
+    //In order to best do this, I recommend having main return a "Result<(), std::io::Error>"
+    //This allows us to immediately exit if it runs into any issues with current dir
+    //We need to end with Ok(()) to signal that the program has exited successfully
+
+    //we can run this in the testing folder with "./../src/main"
+    //Also, to ignore compiler warnings, we can compile with the -Awarnings flag
+    //"rustc main.rs -Awarnings" in src
+
+    //Following that, lets try getting the contents of the dir and printing it out
+    println!("CWD: {}", cwd.display());
+    parse_dir(cwd);
+
+    Ok(())
 }
